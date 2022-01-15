ef7fcf4cd1f44f60ae6af5a56df8c373a5c64f55
Pedrofelipem <felipexexeu02@gmail.com>
2022-01-15 12:03:41 -0300

    Login: colocando componentes na tela

--- src/screens/login/login.tsx
+++ src/screens/login/login.tsx
@@ -1,12 +1,29 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { TouchableOpacity } from "react-native";
+import {
+  Container,
+  Logo,
+  Conteudo,
+  Bemvindo,
+  Recuperarsenha,
+  Textcriarconta,
+  Criarconta,
+} from "../../styles/styles-login";
 
 export interface LoginScreenProps {}
 
 export function LoginScreen(props: LoginScreenProps) {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <Container>
+      <Conteudo>
+        <Logo source={require("../../assets/image/logo.png")} />
+        <Bemvindo>BEM VINDO</Bemvindo>
+        <Recuperarsenha>Recuperar senha</Recuperarsenha>
+        <Textcriarconta>Não tem conta?</Textcriarconta>
+        <TouchableOpacity>
+          <Criarconta>Crie uma nova conta agora</Criarconta>
+        </TouchableOpacity>
+      </Conteudo>
+    </Container>
   );
 }
---
+++ src/styles/styles-login.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  background-color: white;
+  height: 100%;
+  width: 100%;
+`;
+export const Conteudo = styled.View`
+  align-items: center;
+`;
+
+export const Logo = styled.Image`
+  height: 100px;
+  width: 200px;
+`;
+
+export const Bemvindo = styled.Text`
+  font-sinze: 20px;
+`;
+
+export const Recuperarsenha = styled.Text`
+  font-sinze: 20px;
+`;
+export const Textcriarconta = styled.Text`
+  font-sinze: 20px;
+`;
+
+export const Criarconta = styled.Text`
+  font-sinze: 20px;
+`;
